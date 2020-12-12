0a3716ee7e883e1a24fe7b5044054093da52cb7d
Artur Jarosz <azjarosz@gmail.com>
2020-12-12 09:05:33 +0100

    TA-30 split application into modules
    - core module added
    - database module added
    - sample data module aded

---
+++ core/src/main/java/com/arturjarosz/task/sharedkernel/configuration/BaseConfiguration.java
@@ -0,0 +1,44 @@
+package com.arturjarosz.task.sharedkernel.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class BaseConfiguration {
+
+    private final String defaultLanguageCode;
+    private final String defaultEncoding;
+    private final String[] messageSources = {
+            "classpath:/i18n/base/base",
+            "classpath:/i18n/client/client",
+            "classpath:/i18n/architect/architect",
+    };
+
+    public BaseConfiguration(@Value("${task.language}") String language,
+                             @Value("${task.default_encoding}") String encoding) {
+        this.defaultLanguageCode = language;
+        this.defaultEncoding = encoding;
+    }
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource bundleMessageSource = new ReloadableResourceBundleMessageSource();
+        bundleMessageSource.setBasenames(this.messageSources);
+        bundleMessageSource.setDefaultEncoding(this.defaultEncoding);
+        Locale.setDefault(Locale.forLanguageTag(this.defaultLanguageCode));
+        return bundleMessageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        return localeResolver;
+    }
+
+}
