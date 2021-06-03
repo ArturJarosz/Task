a8f7fd1e69c7756d972e7a556a6bfa9e34e6f962
Artur Jarosz <azjarosz@gmail.com>
2021-06-03 21:37:29 +0200

    TA-175 Add FinancialData object and placeholder file for user properties

---
+++ core/src/main/java/com/arturjarosz/task/configuration/YamlPropertiesFactory.java
@@ -0,0 +1,21 @@
+package com.arturjarosz.task.configuration;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
+
+import java.util.Properties;
+
+public class YamlPropertiesFactory implements PropertySourceFactory {
+    @Override
+    public PropertySource<?> createPropertySource(String name, EncodedResource encodedResource) {
+        YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+        factory.setResources(encodedResource.getResource());
+
+        Properties properties = factory.getObject();
+
+        return new PropertiesPropertySource(encodedResource.getResource().getFilename(), properties);
+    }
+}
