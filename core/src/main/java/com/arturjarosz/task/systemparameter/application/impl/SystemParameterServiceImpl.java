8a680d8dfd9c602b3b2eadd26bd04926af194fce
Artur Jarosz <azjarosz@gmail.com>
2021-11-27 10:42:39 +0100

    TA-198 Add todos in the code for implementing methods in system parameters service

--- core/src/main/java/com/arturjarosz/task/systemparameter/application/impl/SystemParameterServiceImpl.java
+++ core/src/main/java/com/arturjarosz/task/systemparameter/application/impl/SystemParameterServiceImpl.java
@@ -24,30 +24,33 @@ public class SystemParameterServiceImpl implements SystemParameterService {
     @Override
     public SystemParameterDto createSystemParameter(SystemParameterDto systemParameterDto,
                                                     SystemParameter systemParameter) {
-        //TODO: validate system parameter dto
-        //TODO: create new system parameter
-        //TODO: add system parameter
+        //TODO TA-198 - validate system parameter dto
+        //TODO TA-198 - create new system parameter
+        //TODO TA-198 - add system parameter
         this.systemParameterRepository.save(systemParameter);
         return null;
     }
 
     @Override
     public SystemParameterDto updateSystemParameter(Long systemParameterId, SystemParameterDto systemParameterDto) {
+        //TODO TA-198 - implement updating system parameters - should it be possible all the time ?
         return null;
     }
 
     @Override
     public void removeSystemParameter(Long systemParameterId) {
-
+        //TODO TA-198 - implement how to remove system parameters
     }
 
     @Override
     public SystemParameterDto getSystemParameter(Long systemParameterId) {
+        //TODO TA-198 - implement retrieving system parameter
         return null;
     }
 
     @Override
     public List<SystemParameterDto> getSystemParameters() {
+        //TODO TA-198 - retrieving all system parameters
         return null;
     }
 }
