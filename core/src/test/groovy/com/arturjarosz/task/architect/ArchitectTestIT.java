d9a5fcf680462848bd133f394c6a08d8a0f0330d
Artur Jarosz <azjarosz@gmail.com>
2021-12-07 23:20:27 +0100

    Remove testcontainers

--- core/src/test/groovy/com/arturjarosz/task/architect/ArchitectTestIT.java
+++
@@ -1,9 +0,0 @@
-package com.arturjarosz.task.architect;
-
-import org.testcontainers.spock.Testcontainers;
-import spock.lang.Specification;
-
-@Testcontainers
-public class ArchitectTestIT extends Specification {
-
-}
